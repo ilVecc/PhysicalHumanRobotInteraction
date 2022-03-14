6c94e9ea7a28124ab38d6402067f1e64f969cc6d
Sebastiano Fregnan <sebastiano@fregnan.me>
2022-03-14 13:18:45 +0100

    Final touch-ups
    Added some result images for the README file
    Small errors in the comments of the robot setup file
    Added boundary layers slope sizes in the teleop setup file
    Added useless zero-order filter for the human position sensing for consistency
    
    [FIXME] human now uses Xm and dXm, but Xs and dXs must be used!
    [TODO] must update the 2DOF scenario, which is an old copy of the UR5 scenario

--- 6dof/setup_robot.m
+++ 6dof/setup_robot.m
@@ -4,11 +4,12 @@ addpath('scripts');
 dh.issym = false;
 dh.dof = 6;
 dh.dof_task = 6;
+% DH table
 dh.d = [0.089159 0 0 0.10915 0.09465 0.0823];
-dh.m = [3.7000 8.3930 2.2750 1.2190 1.2190 0.1879];
 dh.alpha = [pi/2 0 0 pi/2 -pi/2 0];
 dh.a = [0 -0.42500 -0.39225 0 0 0];
-% Coriolis matrix
+% Mass and Centers of mass
+dh.m = [3.7000 8.3930 2.2750 1.2190 1.2190 0.1879];
 cm1 = [0.0 -0.02561 0.00193];
 cm2 = [0.2125 0.0 0.11336];
 cm3 = [0.15 0.0 0.0265];
@@ -17,7 +18,7 @@ cm5 = [0.0 0.0018 0.01634];
 cm6 = [0.0 0.0 -0.001159];
 dh.cm = [cm1' cm2' cm3' cm4' cm5' cm6'];
 clear cm1 cm2 cm3 cm4 cm5 cm6;
-% Inertial tensor
+% Inertial tensor at the centers of mass
 i1 = [0.010267 0.010267 0.00666];
 i2 = [0.2269 0.2269 0.0151];
 i3 = [0.0312168 0.0312168 0.004095];
@@ -40,12 +41,12 @@ gravity = [0 0 9.81]';
 % initial configuration
 conf_m.init.q0 = [-1.6007 -1.7271 -2.203 0.7628 1.5951 -0.031]';
 conf_m.init.dq0 = [0 0 0 0 0 0]';
-conf_m.init.x0 = K_rne(dh, conf.init.q0);
+conf_m.init.x0 = K_rne(dh, conf_m.init.q0);
 conf_m.init.dx0 = [0 0 0 0 0 0]';
 
 conf_s.init.q0 = [-1.6007 -1.7271 -2.203 0.7628 1.5951 -0.031]';
 conf_s.init.dq0 = [0 0 0 0 0 0]';
-conf_s.init.x0 = K_rne(dh, conf.init.q0);
+conf_s.init.x0 = K_rne(dh, conf_s.init.q0);
 conf_s.init.dx0 = [0 0 0 0 0 0]';
 
 % desired configuration
--- 6dof/setup_teleop.m
+++ 6dof/setup_teleop.m
@@ -5,26 +5,31 @@ T_m2s = 25 / 1000 / Ts;  % [ 0.025 s]
 T_s2m = 25 / 1000 / Ts;  % [ 0.025 s]
 
 %% HUMAN SETUP
+% trajectory waypoints
+td  = [              0              1                                  10             18 ];
+Xd1 = [ conf_m.init.x0 conf_m.init.x0  conf_m.init.x0+[0.1;0.0;0.0;0;0;0] conf_m.init.x0 ];
+Xd2 = [ conf_m.init.x0 conf_m.init.x0 conf_m.init.x0+[-0.1;0.1;0.1;0;0;0] conf_m.init.x0 ];
+Xd = Xd2;
 % trajectory controller
 KD_h = eye(dh.dof_task) * 50.0;
 KP_h = eye(dh.dof_task) * 350.0;
-clear z wn;
-% trajectory waypoints
-Xd = [ conf_m.init.x0 conf_m.init.x0 conf_m.init.x0+[0.1;0.0;0.0;0;0;0] conf_m.init.x0 ];
-%Xd = [ conf_m.init.x0 conf_m.init.x0 conf_m.init.x0                 conf_m.init.x0 ];
-td = [            0            1                          10            18 ];
 % human sensing filter
 fc = 10;  % [Hz]
 wc = 2*pi*fc;
-Fh_B = [0 0 0 0 wc^4];
-Fh_A = [1 4*wc 6*wc^2 4*wc^3 wc^4];
+FdXh_B = [0 0 0 0 wc^4];
+FdXh_A = [1 4*wc 6*wc^2 4*wc^3 wc^4];
+clear fc wc;
+fc = 60;  % [Hz]
+wc = 2*pi*fc;
+FXh_B = [1];
+FXh_A = [1];
 clear fc wc;
 
 %% ENVIRONMENT SETUP
 KD_e = zeros(dh.dof_task); KD_e(1,1) = 10;
 KE_e = zeros(dh.dof_task); KE_e(1,1) = 100;
 % environment position
-Xe = conf.init.x0;
+Xe = conf_s.init.x0;
 Xe(1) = -0.08;
 
 %% observations on the the observer's parameters
@@ -63,6 +68,7 @@ K_m = diag(ones(1,dh.dof_task)) * 0;
 % Fm_B = [0 0 wc^2];
 % Fm_A = [1 2*wc wc^2];
 % clear Fm_fc wc
+bl_obs_m = 1e-4;
 
 
 %% SLAVE SETUP
@@ -82,3 +88,5 @@ clear k_g
 % Fs_B = [0 0 wc^2];
 % Fs_A = [1 2*wc wc^2];
 % clear Fs_fc wc
+bl_obs_s = bl_obs_m;
+bl_s = 1e-1;
